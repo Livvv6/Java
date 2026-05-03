a7ba4f8b70a2610b88591be12aad8958877f1727
Liv <livvv803@gmail.com>
2026-05-03 09:51:48 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,21 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import android.os.Bundle;
-import android.widget.Button;
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.EdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-    Button btn;
+class MainActivity : AppCompatActivity() {
+    var btn: Button? = null
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        EdgeToEdge.enable(this)
+        setContentView(R.layout.activity_main)
         //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
         //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
         //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
