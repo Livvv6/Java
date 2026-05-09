21c39aee812b1c4f8dfa7085d3f1c109e35b9c47
Liv <livvv803@gmail.com>
2026-05-09 20:08:21 -0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,15 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.Insets
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    var btn: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
